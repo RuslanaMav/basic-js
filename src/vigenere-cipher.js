3d54ac9db90079cd71616547803456de6e30fe3a
RuslanaMav <ruslana.mav@yandex.ru>
2023-12-21 01:14:28 +0500

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.alp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    if(!str || !key) throw new Error ('Incorrect arguments!');
+    let s = '';
+    let j = 0;
+    key = ''.padStart(str.length, key);
+    for (let i = 0; i < str.length; i++) {
+      if (this.alp.indexOf(str[i].toUpperCase()) === -1) s += str[i];
+      else {
+        s += this.alp.at((this.alp.indexOf(str[i].toUpperCase()) + this.alp.indexOf(key[j].toUpperCase())) % 26);
+        j++;
+      }
+    }
+    return this.type ? s : s.split('').reverse().join('');
+  }
+
+  decrypt(str, key) {
+    if(!str || !key) throw new Error ('Incorrect arguments!');
+    let s = '';
+    let j = 0;
+    key = ''.padStart(str.length, key);
+    for (let i = 0; i < str.length; i++) {
+      if (this.alp.indexOf(str[i].toUpperCase()) === -1) {
+        s += str[i];
+        continue;
+      }
+      let str1 = this.alp.indexOf(str[i].toUpperCase()) - this.alp.indexOf(key[j].toUpperCase());
+      if (str1 >= 0) s += this.alp.at(str1);
+      else s += this.alp.at(str1 + 26);
+      j++;
+    }
+    return this.type ? s : s.split('').reverse().join('');
   }
 }
 
