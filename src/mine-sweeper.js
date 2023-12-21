446774504c6f7cf2f094b85cb682d32da60e1df6
RuslanaMav <ruslana.mav@yandex.ru>
2023-12-21 19:41:07 +0500

    feat: add solution

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -24,9 +24,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]
  */
 function minesweeper(matrix) {
-  throw new NotImplementedError('Not implemented');
+  let n = matrix.length; 
+  let m = matrix[0].length; 
+  let matrix1 = matrix.map(item => item.slice());
 
-  
+  for (let a = 0; a < n; a++) {
+    for (let b = 0; b < m; b++) {
+      let count = 0;
+      if (b !== 0 && matrix[a][b - 1]) count++;
+      if (b !== (m - 1) && matrix[a][b + 1]) count++;
+      if (b !== 0 && a !== (n - 1) && matrix[a + 1][b - 1]) count++;
+      if (a !== (n - 1) && matrix[a + 1][b]) count++;
+      if (a !== (n - 1) && b !== (m - 1) && matrix[a + 1][b + 1]) count++;
+      if (b !== 0 && a !== 0 && matrix[a - 1][b - 1]) count++;
+      if (a !== 0 && matrix[a - 1][b]) count++;
+      if (a !== 0 && b !== (m - 1) && matrix[a - 1][b + 1]) count++;
+      matrix1[a][b] = count;
+    }
+  }
+  return matrix1;
 }
 
 module.exports = {
